7296baa463652992ff51b54bc78b63a27d22065e
Usman3578 <UsmanDialog@PC11789>
2018-11-02 15:35:14 +1100

    Adding base classes

---
+++ Application.Domain/Base/EntityBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Domain.Base
+{
+    public abstract class EntityBase
+    {
+
+    }
+
+    public abstract class Entity : EntityBase, IEntity
+    {
+        public virtual int Id { get; set; }
+    }
+}
